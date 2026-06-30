9e28f8f045b69a4bdc79f4f11706829593e89f7b
Fred <frederik.koenig@posteo.de>
2026-06-30 14:03:48 +0200

    fixed presentation mode error in client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,18 +1,18 @@
 import { createClient } from "next-sanity";
-
 import { apiVersion, dataset, projectId } from "../env";
-import type { QueryParams } from "next-sanity";
+import { draftMode } from "next/headers";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: false,
   token: process.env.SANITY_VIEWER_TOKEN,
   stega: {
     studioUrl: process.env.NEXT_PUBLIC_SANITY_STUDIO_URL,
   },
 });
+
 export const previewClient = client.withConfig({
   perspective: "previewDrafts",
   useCdn: false,
@@ -20,8 +20,16 @@ export const previewClient = client.withConfig({
 
 export async function sanityProductionFetch<QueryResult>(
   query: string,
-  params: QueryParams = {},
-  tags: string[],
+  params: Record<string, unknown> = {},
+  tags: string[] = [],
 ): Promise<QueryResult> {
+  const isDraftMode = (await draftMode()).isEnabled;
+
+  if (isDraftMode) {
+    return previewClient.fetch<QueryResult>(query, params, {
+      cache: "no-store",
+    });
+  }
+
   return client.fetch<QueryResult>(query, params, { next: { tags } });
 }
